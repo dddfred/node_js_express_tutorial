bdf9ee6e04c2d5f150fcb49175c6f3d3cd8038ef
Fredy Diaz <fredy.diaz@stu.bmcc.cuny.edu>
2025-03-11 23:21:47 -0400

    "added async patters and HTTP file"

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
+const _ = require('lodash');
+
+const items = [1,[2, [4]]];
+
+const newItems = _.flatten(items);
+
+console.log(newItems);
+
+console.log('Hello World!');
+
 /**
- *  The module http has a function to create a server called createServer()
+ * LOCAL from a repo
+ * The package.json has a list of all the dependecies that the project needs, it is up to us to install those dependencies by running the command `npm install`. This will install all dependencies needed.
  * 
- * The function requires a callback function with two paramenters
- * The first paramenter is the incoming request
- * The second paramenter is the response that we want to take for the incoming request
+ * To unistall a package, we can use the command `npm unistall <packageName>`
  * 
- * We also have to set up what port is our server listening on with a listener() method
+ * We have a nuclear approach to unistall a package, which involves deleting the node_modules folder and the package-lock.json file.
+ * We can simply delete the folder from the side bar or run this command `rm -rf node_modules`
+ * We can also simple delete the package-lock.json file from the side bar.
+ * DO NOT FORGET to remove the depencie from the dependencies list in the package.json if you dont want 
+ * this dependencie anymore, otherwise, you'll reinstall said dependencie.
+ * After this, we can run `npm install` to reinstall desired packages.
+ * You may want to do this in case you get some errors like permission errors or faulty packages.
+ * 
+ * LOCAL first time
+ * To install new packages onto your project for the first time, we can do so by typing the command
+ *  `npm i <packageName>`
+ * 
+ * GLOBAL
+ * You can install packages globally, instead of locally by applying the `-g` flag
+ * You may want to do this if you have a package that you use constantly and is advantageous to globally install it
+ * 
+ * The full command on mac to install a package globally is `sudo npm install -g <packageName>`
+ * 
+ * Many of the packages cause issues when installed globally
+ * 
+ * it is better to use npx
+ *
+ * 
+ * why package.json>?
+ * Many of the dependencies have versions, package.json includes the version of the dependencie so if a 
+ * package update its version and gets rid of some features, our project wont be affected and there will 
+ * no errors
+ * 
  */
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.end('Welcome to our home page');
-    }
-    if(req.url === '/about'){
-        res.end('This is a short history about us');
-    }
-
-    res.end(`
-        <h1>Ooops!</h1>
-        <p>We can't seem to find the page that you are looking for</p>
-        <a href="/"> Back home! </a>
-    `)
-})
-
-server.listen(5000);
